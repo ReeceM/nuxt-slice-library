530b47f0e733c809b78e4f3d3604025c26c049b2
ReeceM <reecestevenmay@gmail.com>
2020-11-22 23:08:46 +0200

    feat: some components

---
+++ components/Container/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <component
+    :is="'section'"
+    class="container mx-auto px-5 py-24">
+    <slot/>
+  </component>
+</template>
+
+<script>
+export default {
+  props: {
+    propName: {
+      type: Number,
+      default: () => {}
+    },
+  },
+}
+</script>
---
+++ components/Eyebrow/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <component
+    :is="'span'"
+    class="uppercase mb-6 text-xs font-semibold"
+    :class="theme.color ? theme.color : 'text-green-500'">
+    <slot />
+  </component>
+</template>
+
+<script>
+  import {
+    commonProps
+  } from '../../utils'
+
+  export default {
+    name: 'Eyebrow',
+    props: {
+      ...commonProps,
+    },
+  }
+
+</script>
+
+<style>
+
+</style>
