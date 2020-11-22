559bd2d962bd2c540b70dba38e57b455712af561
ReeceM <reecestevenmay@gmail.com>
2020-11-22 23:25:56 +0200

    feat: footer

---
+++ slices/FooterSection/Links.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+    <prismic-rich-text
+      :field="currentTitle"
+      class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3"
+    />
+
+    <nav class="list-none mb-10">
+      <li v-for="(item, index) in links" :key="index">
+        <prismic-link
+          :field="item.link"
+          class="text-gray-600 hover:text-gray-800 transform hover:translate-x-1 hover:translate-y-3">
+          <span v-text="item.linkLabel" />
+        </prismic-link>
+      </li>
+    </nav>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      links: {
+        type: Array,
+        default: () => {
+          return [];
+        }
+      },
+    },
+    computed: {
+      currentTitle() {
+        return this.links[0].title ?? '';
+      }
+    },
+  }
+
+</script>
+
---
+++ slices/FooterSection/index.stories.js
@@ -0,0 +1,40 @@
+import Slice from './';
+import model from './model';
+import mocks from './mocks.json';
+import overrides from './overrides.json';
+import SliceZone from 'vue-slicezone';
+
+export default {
+  title: model.name,
+};
+
+const mocked = mocks[0]
+mocked.items = overrides.items;
+mocked.primary.copyrightNotice = overrides.primary.copyrightNotice;
+
+// mocked.items = mocked.items.map(item => {
+//   const title = item.title;
+//   title[0].text = title[0].text.split(' ')[0];
+//   item.title = title;
+
+//   return item;
+// })
+
+// TODO: Update to loop over mocks.json
+export const DefaultSlice = () => ({
+  components: {
+    Slice,
+    SliceZone,
+  },
+  data() {
+    return {
+      mock: mocked,
+      resolver() {
+        return Slice;
+      }
+    };
+  },
+  template: '<slice-zone :slices="[ mock ]" :resolver="resolver" />',
+});
+
+DefaultSlice.storyName = mocks[0].name;
