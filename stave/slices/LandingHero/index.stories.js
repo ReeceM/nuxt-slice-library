a5b2f0dacf536c709d4c7be75fa9339166105054
ReeceM <reecestevenmay@gmail.com>
2020-11-23 23:36:12 +0200

    fix landing

--- stave/slices/LandingHero/index.stories.js
+++ stave/slices/LandingHero/index.stories.js
@@ -1,10 +1,17 @@
+import {
+  withKnobs,
+  text,
+} from "@storybook/addon-knobs";
+
 import Slice from './';
 import model from './model';
 import mocks from './mocks.json';
 import SliceZone from 'vue-slicezone';
+import { cloneDeep } from "lodash";
 
 export default {
   title: model.name,
+  decorators: [withKnobs]
 };
 
 // TODO: Update to loop over mocks.json
@@ -15,13 +22,80 @@ export const DefaultSlice = () => ({
   },
   data() {
     return {
-      mock: mocks[0],
       resolver() {
         return Slice;
       }
     };
   },
+  props: {
+    mock: {
+      default: (() => {
+        const _mock = cloneDeep(mocks[0]);
+
+        _mock.primary.title = text(
+          'Title',
+          _mock.primary.title
+        );
+        _mock.primary.description[0].text = text(
+          'Descirption',
+          'And a really long description about things that should be here'
+        );
+
+        _mock.primary.image.url = text(
+          'Image',
+          'https://images.prismic.io/nuxt-slice-library/92143963-d3f2-4efd-945c-b3ee0d79947e_cassi-josh-lhnOvu72BM8-unsplash.jpg?auto=compress,format'
+        );
+
+        _mock.primary.ctaLabel = text(
+          'CTA Text',
+          'Primary Action'
+        );
+
+        _mock.primary.ctaLink.url = text(
+          'CTA Link',
+          _mock.primary.ctaLink.url
+        );
+
+        _mock.primary.secondaryLabel = text(
+          'Secondary Text',
+          'Secondary Action'
+        );
+
+        _mock.primary.secondaryLink.url = text(
+          'Secondary Link',
+          _mock.primary.secondaryLink.url
+        );
+
+        return _mock;
+      })()
+    },
+    // theme: {
+    //   default: (() => {
+    //     const _theme = {
+    //       eyebrow: {
+    //         color: 'text-green-500'
+    //       }
+    //     }
+
+    //     _theme.eyebrow.color = text(
+    //       'Eyebrow Color',
+    //       'text-green-500'
+    //     );
+
+    //     return _theme
+    //   })()
+    // }
+  },
   template: '<slice-zone :slices="[ mock ]" :resolver="resolver" />',
 });
+
+DefaultSlice.story = {
+  parameters: {
+    knobs: {
+      escapeHTML: false
+    }
+  }
+}
+
 
 DefaultSlice.storyName = mocks[0].name;
--- stave/slices/LandingHero/index.vue
+++ stave/slices/LandingHero/index.vue
@@ -1,11 +1,31 @@
 <template>
   <div :class="theme.hero_text_color">
     <container>
-      <div class="container mx-auto">
-        <prismic-rich-text :field="slice.primary.title" class="title" />
-        <prismic-rich-text :field="slice.primary.description" />
-        <!-- <prismic-image class="" :field="slice.primary.image" /> -->
-      </div>
+      <section class="text-gray-700 body-font">
+        <div class="flex px-5 -py-1 md:flex-row flex-col items-center">
+          <div
+            class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <prismic-rich-text class="text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl"
+              :field="slice.primary.title" />
+            <prismic-rich-text class="mb-8 leading-relaxed" :field="slice.primary.description" />
+            <div class="flex justify-center md:space-x-3" v-if="slice.items">
+              <prismic-link
+                class="inline-flex py-2 px-6 rounded text-lg cursor-pointer text-white bg-green-400 hover:bg-green-600"
+                :field="slice.primary.ctaLink">
+                {{slice.primary.ctaLabel}}
+              </prismic-link>
+              <prismic-link
+                class="inline-flex py-2 px-6 rounded text-lg cursor-pointer text-green-700 border-green-300 border-2 hover:bg-green-400 hover:text-white "
+                :field="slice.primary.secondaryLink">
+                {{slice.primary.secondaryLabel}}
+              </prismic-link>
+            </div>
+          </div>
+          <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <prismic-image class="object-cover object-center rounded" :field="slice.primary.image"></prismic-image>
+          </div>
+        </div>
+      </section>
     </container>
   </div>
 </template>
@@ -23,13 +43,6 @@
     },
     props: {
       ...commonProps,
-      slice: {
-        type: Object,
-        required: true,
-        default () {
-          return {}
-        }
-      }
     },
   }
 
