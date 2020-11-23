0145bf5bc6e047c0e92fe0eef8c0b56cbed003b9
ReeceM <reecestevenmay@gmail.com>
2020-11-23 20:26:11 +0200

    home page demo

--- pages/index.vue
+++ pages/index.vue
@@ -7,11 +7,72 @@ https://www.slicemachine.dev/documentation/nuxt/add-the-slice-zone-to-your-page
 </template>
 
 <script>
-import SliceZone from "vue-slicezone";
+  import SliceZone from "vue-slicezone";
 
-export default {
-  components: {
-    SliceZone
+  export default {
+    components: {
+      SliceZone
+    },
+    head() {
+      const {
+        meta_title,
+        meta_description,
+        social_cards : [{
+          social_card_image,
+          social_card_title,
+          social_card_description
+        }]} = this.data;
+
+      return {
+        title: meta_title,
+        meta: [{
+          hid: "description",
+          name: "description",
+          content: meta_description
+        },
+        {
+          hid: "twitter:image",
+          name: "twitter:image",
+          content: social_card_image.url
+        },
+        {
+          hid: "twitter:title",
+          name: "twitter:title",
+          content: social_card_title
+        },
+        {
+          hid: "twitter:description",
+          name: "twitter:description",
+          content: social_card_description
+        },
+        {
+          hid: "og:image",
+          name: "og:image",
+          content: social_card_image.url
+        },
+        {
+          hid: "og:title",
+          name: "og:title",
+          content: social_card_title
+        },
+        {
+          hid: "og:description",
+          name: "og:description",
+          content: social_card_description
+        },
+        {
+          hid: "twitter:image",
+          name: "twitter:image",
+          content: "@iexistin3d"
+        },
+        ]
+      }
+    },
+    async asyncData(context) {
+      const data = (await context.$prismic.api.getByUID("page", "home")).data;
+
+      return { data };
+    },
   }
-};
+
 </script>
